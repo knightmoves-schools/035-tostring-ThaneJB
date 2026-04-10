b3a1bc64a3ba3f95fd47a237aaec049a9d519034
Thane <thabay0606@students.maquoketaschools.org>
2026-04-10 12:26:25 -0500

    Should return proper items.

--- Person.cs
+++ Person.cs
@@ -12,4 +12,7 @@ public class Person {
    }
 
    // Add your code here
+   public override string? ToString(){
+      return $"first name: {firstName}, last name: {lastName}, age: {age}";
+   }
 }
